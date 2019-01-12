4282e91b6ee16ea5fa0791627d9b87f11d4ec943
Leonardo Matos <leomp120894@gmail.com>
2019-01-12 19:36:24 -0200

    no borders, small gap

--- .config/awesome/themes/zenburn/theme.lua
+++ .config/awesome/themes/zenburn/theme.lua
@@ -25,8 +25,8 @@ theme.bg_systray = theme.bg_normal
 -- }}}
 
 -- {{{ Borders
-theme.useless_gap   = dpi(0)
-theme.border_width  = dpi(2)
+theme.useless_gap   = dpi(1)
+theme.border_width  = dpi(0)
 theme.border_normal = "#3b4045"
 theme.border_focus  = "#232627"
 theme.border_marked = "#3daee9"
