26939e258d2abcb1d67495e1efc808b31f5470dd
Leonardo Matos <leomp120894@gmail.com>
2019-01-12 18:32:47 -0200

    sellect customized theme file

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -41,7 +41,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "zenburn/theme.lua")
+beautiful.init(awful.util.getdir("config") .. "/themes/zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "konsole"
